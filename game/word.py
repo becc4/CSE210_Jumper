82d86a368f8f4f26c7f3bdc1c0d577bb9e44aa31
caleb <caleb.malcarne@gmail.com>
2022-06-22 09:46:50 -0600

    uploaded word

--- game/word.py
+++ game/word.py
@@ -1,2 +1,20 @@
+import random
+
 class word():
-    pass
+    
+    def __init__(self):
+        
+        _word_Len = 0
+        _chosen_Word = ''
+
+    def gen_Word(self):
+        _wordList = ["clock","spray","index","paint","smash","aware","theme","ranch","grass","solve","album",
+                    "elbow","worry","fence","house","basis","trait","piano","stain","thumb","plane","merit","print",
+                    "woman","dream","child","tasty","glass","climb","layer","watch","snarl","giant","beach","money",
+                    "Bible","joint","issue","image","proof","plant","metal","night","catch","pride","knock","chord",
+                    "alive","force","clash","car","cat","dog","bat","fat","set", "is", "at", "or", "if"]
+        
+        self._chosen_Word = _wordList[random.randint(0,len(_wordList))]
+
+    def word_Len(self):
+        return self._chosen_Word.len()
