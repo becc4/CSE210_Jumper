02b57fd3b7395553ee40665a5de49174bec03bb5
Logan <logan.hitchcock54@gmail.com>
2022-06-23 10:01:03 -0600

    commit

---
+++ Jumper/game/Comparison.py
@@ -0,0 +1,18 @@
+
+class comparison():
+
+    def compare(word,user_input):
+        list_of_text = list(word)
+        letter = user_input
+
+        for letters in list_of_text:
+            pass
+
+
+        #compares if the user input matches a letter in the word 
+
+        #returns either true or false 
+
+
+
+
