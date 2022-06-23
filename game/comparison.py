f2c7edb1819fc5c105ee7d5a5a196de6aa50d832
Logan <logan.hitchcock54@gmail.com>
2022-06-23 09:43:11 -0600

    commit

---
+++ game/comparison.py
@@ -0,0 +1,31 @@
+
+class comparison():
+
+    def compare(word,user_input):
+        list_of_text = list(word)
+
+
+        #compares if the user input matches a letter in the word 
+
+        #returns either true or false 
+
+
+
+
+    
+
+
+
+
+
+#compares if the letter input from the user matches a letter in the word
+
+#get the guess from "director"
+
+#is it correct?
+
+    #if yes:
+        #return true
+
+    #else:
+        #return false
